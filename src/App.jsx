09060aac06ac068b91b2fd69fe6fd9dd1e09bdf5
Luis Matos <luismatos138@gmail.com>
2023-05-17 16:02:12 +0100

    Solved Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const setCountToZero = () => {
+    setCount(0);
+  };
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+  const randomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Number Counter</h1>
+      <h2>My first React app</h2>
+      <h3>Counter : {count}</h3>
+      <button
+        onClick={() => {
+          if (count < 100) {
+            incrementCount();
+          }
+        }}
+      >
+        Increment
+      </button>
+      <button onClick={setCountToZero}>Reset</button>
+      <button
+        onClick={() => {
+          if (count > 0) {
+            decrementCount();
+          }
+        }}
+      >
+        Decrement
+      </button>
+      <button onClick={() => setCount(randomNum(1, 100))}>Random</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
